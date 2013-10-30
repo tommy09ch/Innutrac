b850c2cbe28813cc4edcf33b138e98b8543a5d53
Tommy (Trung) Huynh <tommy09ch@gmail.com>
2013-10-30 13:35:07 -0700

    export user info to a file

--- src/com/innutrac/poly/innutrac/GetUserInfo.java
+++ src/com/innutrac/poly/innutrac/GetUserInfo.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import android.app.Activity;
@@ -13,7 +14,9 @@ import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class GetUserInfo extends Activity {
 	boolean flag = false;
@@ -21,30 +24,23 @@ public class GetUserInfo extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.get_user_info);
-
 		final RadioButton maleRB = (RadioButton) findViewById(R.id.profMaleRButton);
 		final RadioButton femaleRB = (RadioButton) findViewById(R.id.profFemaleRButton);
 
 		maleRB.setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				maleRB.setChecked(true);
 				femaleRB.setChecked(false);
 			}
-
 		});
 
 		femaleRB.setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				maleRB.setChecked(false);
 				femaleRB.setChecked(true);
 			}
-
 		});
 
 		Button save = (Button) findViewById(R.id.save_user_button);
@@ -52,38 +48,69 @@ public class GetUserInfo extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				String gender = "";
-				String name = "";
-				String age = "";
+				String gender = null;
+				String name = null;
+				String age = null;
 
-				name = findViewById(R.id.profileNameField).toString();
-				age = findViewById(R.id.profAgeField).toString();
-				if (findViewById(R.id.profMaleRButton).isSelected()) {
+				name = ((EditText) findViewById(R.id.profileNameField))
+						.getText().toString();
+				age = ((EditText) findViewById(R.id.profAgeField)).getText()
+						.toString();
+				if (maleRB.isChecked()) {
 					gender = "M";
-				} else {
+				} else if (femaleRB.isChecked()) {
 					gender = "F";
 				}
 
-				Person person = new Person();
-				person.setAge(age);
-				person.setName(name);
-				person.setGender(gender);
-				
-				
-				Intent i = new Intent(GetUserInfo.this, MainActivity.class);
-				startActivity(i);
+				if (name.isEmpty() || gender.isEmpty() || age.isEmpty()) {
+					Toast.makeText(GetUserInfo.this, "Please complete all fields", Toast.LENGTH_LONG).show();
+				} else {
+					saveToFile(name, age, gender);
+					
+					Toast.makeText(GetUserInfo.this, "Profile Created", Toast.LENGTH_LONG).show();
+					Intent i = new Intent(GetUserInfo.this, MainActivity.class);
+					startActivity(i);
+				}
 			}
 		});
 
-	
 		Button skip = (Button) findViewById(R.id.skip_user_button);
 		skip.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent(GetUserInfo.this, MainActivity.class);
-				startActivity(i);				
-			}});
+				startActivity(i);
+			}
+		});
 
 	}
+
+	public void saveToFile(String n, String a, String g) {
+
+		String sdPath = Environment.getExternalStorageDirectory()
+				.getAbsolutePath();
+		File file = new File(sdPath + "/Innutrac/user_info.txt");
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			FileWriter fw = new FileWriter(file, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(n);
+			bw.write("\n");
+			bw.write(a);
+			bw.write("\n");
+			bw.write(g);
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }

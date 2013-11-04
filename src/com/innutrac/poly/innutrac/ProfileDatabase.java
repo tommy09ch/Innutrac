2b0942ccaec3a04bc34d419673db02a7af53a446
cmr2333 <Chris@Chriss-MacBook-Pro.local>
2013-11-03 22:44:52 -0800

    Initial Database Function
    Class for database interaction with profile. CRUD functions for user
    profiles and food records.

---
+++ src/com/innutrac/poly/innutrac/ProfileDatabase.java
@@ -0,0 +1,133 @@
+package com.innutrac.poly.innutrac;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ProfileDatabase extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME_PROFILE = "profile"; 
+    
+    private static final String TABLE_PROFILE = "profile_info"; 
+    private static final String KEY_PROFILE_ID = "id";
+    private static final String KEY_PROFILE_NAME = "name";
+    private static final String KEY_PROFILE_AGE = "age";
+    private static final String KEY_PROFILE_SEX = "sex";
+    private static final String KEY_PROFILE_CREATE_TIME = "create_time";
+    
+    private static final String TABLE_FOODREC = "food_records"; 
+    private static final String KEY_FOODREC_ID = "id";
+    private static final String KEY_FOODREC_FOODID = "food_id";
+    private static final String KEY_FOODREC_AMOUNT = "amount";
+    private static final String KEY_FOODREC_TIME = "time";
+	
+	public ProfileDatabase(Context context) {
+		super(context, DATABASE_NAME_PROFILE, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		 // SQL statement to create profile table
+        String queryStr = "CREATE TABLE "+TABLE_PROFILE+" ( " +
+        		KEY_PROFILE_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        		KEY_PROFILE_NAME+" TEXT, "+
+        		KEY_PROFILE_AGE+" INTEGER, "+
+        		KEY_PROFILE_SEX+" INTEGER, "+
+        		KEY_PROFILE_CREATE_TIME+" NUMERIC )";
+        db.execSQL(queryStr);
+        //Now create table for storing food entries
+        queryStr = "CREATE TABLE "+TABLE_FOODREC+" ( " +
+        		KEY_FOODREC_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+        		KEY_FOODREC_FOODID+" INTEGER, "+
+        		KEY_FOODREC_AMOUNT+" REAL, "+
+        		KEY_FOODREC_TIME+" NUMERIC )";
+        db.execSQL(queryStr);
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Do nothing. Don't delete existing data!
+	}
+	
+	public void addProfile(String name, int age, boolean sex)
+	{
+		long time = System.currentTimeMillis() / 1000L;
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_PROFILE_NAME, name);
+        values.put(KEY_PROFILE_AGE, age); 
+        values.put(KEY_PROFILE_SEX, sex);
+        values.put(KEY_PROFILE_CREATE_TIME, time); 
+        
+        db.insert( TABLE_PROFILE, null,values );
+        db.close(); 
+	}
+	
+	public int updateProfile(int profile_id, String name, int age, boolean sex) 
+	{
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_PROFILE_NAME, name);
+        values.put(KEY_PROFILE_AGE, age); 
+        values.put(KEY_PROFILE_SEX, sex);
+
+        int i = db.update(TABLE_FOODREC, //table
+                values, // column/value
+                KEY_PROFILE_ID+" = ?", // selections
+                new String[] { String.valueOf(profile_id) }); //selection args
+
+        db.close();
+ 
+        return i;
+    }
+	
+	public void addFood(int foodID, double amount)
+	{
+		addFood(foodID, amount, (System.currentTimeMillis() / 1000L));
+	}
+	
+	public void addFood(int foodID, double amount, long time)
+	{
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_FOODREC_FOODID, foodID);
+        values.put(KEY_FOODREC_AMOUNT, amount); 
+        values.put(KEY_FOODREC_TIME, time);
+        
+        db.insert( TABLE_PROFILE, null,values );
+        db.close(); 
+	}
+	
+	public void deleteFood(int entry_id) 
+	{
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_FOODREC,
+                KEY_FOODREC_ID+" = ?",
+                new String[] { String.valueOf(entry_id) });
+        db.close();
+    }
+	
+	public int updateFood(int entry_id, int foodID, double amount, long time) 
+	{
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_FOODREC_FOODID, foodID);
+        values.put(KEY_FOODREC_AMOUNT, amount); 
+        values.put(KEY_FOODREC_TIME, time);
+
+        int i = db.update(TABLE_FOODREC, //table
+                values, // column/value
+                KEY_FOODREC_ID+" = ?", // selections
+                new String[] { String.valueOf(entry_id) }); //selection args
+
+        db.close();
+ 
+        return i;
+    }
+
+}

ad9aa527c207bfc6dab9e00a5fb8381b21760b46
Tommy (Trung) Huynh <tommy09ch@gmail.com>
2013-11-13 00:35:49 -0800

    add basic implementation of wedge summary activity

--- src/com/innutrac/poly/innutrac/PieViewFragment.java
+++ src/com/innutrac/poly/innutrac/PieViewFragment.java
@@ -1,28 +1,89 @@
 package com.innutrac.poly.innutrac;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-
 
 public class PieViewFragment extends Fragment {
 	LinearLayout pieContainer;
 	int factor = 1;
 	private PieView pie;
-	
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_pie, null);
 	}
-	
+
 	@Override
-	public void onActivityCreated(Bundle savedInstanceState){
+	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-        pieContainer = (LinearLayout) getView().findViewById(R.id.pie_container_id);
-        pie = new PieView(getActivity());
-        pie.setMode(factor);
-        pieContainer.addView(pie);
+		pieContainer = (LinearLayout) getView().findViewById(
+				R.id.pie_container_id);
+		pie = new PieView(getActivity());
+		pie.setMode(factor);
+		pieContainer.addView(pie);
+
+		pieContainer.setOnTouchListener(new View.OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				String name = "";
+				boolean checker = false;
+				float x = event.getX();
+				float y = event.getY();
+				// will send touch location to PieView and appropriately change
+				// to detail view
+				switch (pie.wedgeDetect(x, y)) {
+				case 1:
+					name = "Sodium";
+					checker = true;
+					break;
+				case 2:
+					name = "Carbohydrates";
+					checker = true;
+					break;
+				case 3:
+					name = "Protein";
+					checker = true;
+					break;
+				case 4:
+					name = "Calories";
+					checker = true;
+					break;
+				case 5:
+					name = "Fiber";
+					checker = true;
+					break;
+				case 6:
+					name = "Fat";
+					checker = true;
+					break;
+				case 7:
+					name = "Sugar";
+					checker = true;
+					break;
+				case 8:
+					name = "Cholesterol";
+					checker = true;
+					break;
+				default:
+					checker = false;// do nothing
+				}
+				if (checker) {
+					DailyPlan dp = ((MainActivity) getActivity())
+							.getTodayDailyPlan();
+
+					startActivity(new Intent(getActivity(),
+							WedgeSummaryActivity.class).putExtra("title", name)
+							.putExtra("dp", dp));
+				}
+				return false;
+			}
+		});
 	}
 }
